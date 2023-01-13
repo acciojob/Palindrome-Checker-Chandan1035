76dfbc7ae6a81ca932ca00b4081499b1db500056
Chandan1035 <lchandanrao01@gmail.com>
2023-01-13 08:16:16 +0530

    done

--- solution.js
+++ solution.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
+    str = str.toLowerCase(); 
+    str = str.replace(/[^a-z0-9]/gi, ''); 
+    return str === str.split('').reverse().join('');
 
 }
 module.exports = palindrome
